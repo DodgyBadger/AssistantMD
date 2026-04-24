b0d9e8ba5ee72ea36d890c162e9c9f9ceb7fefd9
DodgyBadger <4453217+DodgyBadger@users.noreply.github.com>
2026-04-24 05:56:19 +0000

    Remove obsolete assemble_context shim

--- core/authoring/helpers/assemble_context.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility shim for the assemble_context helper implementation."""
-
-from core.authoring.helpers.history.assemble import build_definition, execute
-
-__all__ = ["build_definition", "execute"]
